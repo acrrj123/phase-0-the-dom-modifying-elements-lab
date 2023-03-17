9deb519d6095302010134267ac474a6372f4fef4
Anna Cole <acrrj123@gmail.com>
2023-03-16 22:16:44 -0500

    commit tree eb4a034cee7827b165a34de34e9205331418462f

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+//Make sure you create an <h1> and assign it to the 'newHeader' variable"
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.textContent = "Anna is the champion";
